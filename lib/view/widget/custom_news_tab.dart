8ea4d3d026f1402feb4de70ae14541c325b6d947
zwethu <thuraung1313@gmail.com>
2022-09-22 00:11:14 +0630

    loading animation finished

--- lib/view/widget/custom_news_tab.dart
+++ lib/view/widget/custom_news_tab.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'dart:math' as math;
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:news_app/core/colors.dart';
 import 'package:news_app/view/widget/news_list.dart';
 import 'package:news_app/view_model/bloc/online_article_bloc/online_article_bloc.dart';
 
@@ -25,9 +27,9 @@ class _CustomNewsTabState extends State<CustomNewsTab> {
     return BlocBuilder<OnlineArticleBloc, OnlineArticleBlocState>(
       builder: (context, state) {
         if (state is OnlineArticleBlocInitialState) {
-          return const Placeholder(color: Colors.amber);
+          return const LoadingAnimation();
         } else if (state is OnlineArticleBlocLoadingState) {
-          return const Placeholder(color: Colors.red);
+          return const LoadingAnimation();
         } else if (state is OnlineArticleBlocEmptyState) {
           return const Placeholder(color: Colors.green);
         } else if (state is OnlineArticleBlocLoadedState) {
@@ -35,22 +37,88 @@ class _CustomNewsTabState extends State<CustomNewsTab> {
         } else if (state is OnlineArticleBlocErrorState) {
           return const Placeholder();
         } else if (state is OnlineArticleBlocNoConnectionState) {
-          return const Placeholder();
+          return const NoInternetWidget();
         } else {
-          return const Placeholder();
+          return const LoadingAnimation();
         }
       },
     );
   }
 }
 
+class LoadingAnimation extends StatefulWidget {
+  const LoadingAnimation({Key? key}) : super(key: key);
 
+  @override
+  State<LoadingAnimation> createState() => _LoadingAnimationState();
+}
 
+class _LoadingAnimationState extends State<LoadingAnimation>
+    with TickerProviderStateMixin {
+  late Animation<double> animation;
+  late AnimationController animController;
+  bool isEmpty = false;
+  @override
+  void initState() {
+    super.initState();
+    animController = AnimationController(
+      duration: const Duration(seconds: 2),
+      vsync: this,
+    );
+    animation = Tween<double>(
+      begin: 0,
+      end: math.pi,
+    ).animate(animController)
+      ..addListener(() {
+        // Empty setState because the updated value is already in the animation field
+        setState(() {});
+      })
+      ..addStatusListener((status) {
+        if (status == AnimationStatus.completed) {
+          animController.reverse();
+          isEmpty = !isEmpty;
+        } else if (status == AnimationStatus.dismissed) {
+          animController.forward();
+          isEmpty = !isEmpty;
+        }
+      });
+    animController.forward();
+  }
 
+  @override
+  void dispose() {
+    animController.dispose();
+    super.dispose();
+  }
 
+  @override
+  Widget build(BuildContext context) {
+    return Transform.rotate(
+      angle: animation.value,
+      child: Center(
+        child: isEmpty
+            ? const Icon(
+                Icons.hourglass_top_rounded,
+                size: 35,
+                color: themeColor,
+              )
+            : const Icon(
+                Icons.hourglass_bottom_rounded,
+                size: 35,
+                color: themeColor,
+              ),
+      ),
+    );
+  }
+}
 
+class NoInternetWidget extends StatelessWidget {
+  const NoInternetWidget({Key? key}) : super(key: key);
 
-
-
-
-
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Icon(Icons.signal_wifi_bad_rounded,size: 35,color:themeColor,),
+    );
+  }
+}
