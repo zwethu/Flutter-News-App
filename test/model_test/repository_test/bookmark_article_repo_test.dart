45907ed647d462248f4398710a8ef64e8d058ad0
zwethu <thuraung1313@gmail.com>
2022-11-03 14:04:05 +0630

    test bookmarkArticleRepo

--- test/model_test/repository_test/bookmark_article_repo_test.dart
+++ test/model_test/repository_test/bookmark_article_repo_test.dart
@@ -2,22 +2,50 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:hive/hive.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
-import 'package:news_app/core/constants.dart';
 import 'package:news_app/model/hive/article_box.dart';
 import 'package:news_app/model/repository/bookmark_article_repo.dart';
 
 import 'bookmark_article_repo_test.mocks.dart';
 
-
-
-@GenerateMocks([HiveInterface])
+@GenerateMocks([Box<ArticleBox>])
 void main() {
-  group('',()async{
-     MockHiveInterface hive = MockHiveInterface();
-     BookmarkArticleRepo repo = BookmarkArticleRepo(hive.box<ArticleBox>(articleBox));
-    
-    // test('testing', (){
-    //   when(repo.getArticles('')).thenAnswer((_) => );
-    // });
+  MockBox<ArticleBox> box = MockBox<ArticleBox>();
+  BookmarkArticleRepo repo = BookmarkArticleRepo(box);
+  ArticleBox data = ArticleBox()
+    ..author = "author"
+    ..title = "title"
+    ..description = "description"
+    ..url = "url"
+    ..urlToImage = "urlToImage"
+    ..publishedAt = "publishedAt"
+    ..content = "content";
+  group('test getArticles method', () {
+    test('return [] when Hive box is empty.', () {
+      when(box.length).thenAnswer((_) => 0);
+      final result = repo.getArticles('');
+      expect(result, []);
+      verify(repo.getArticles(''));
+    });
+    test('return [<ArticleBox>,...] when hive has data', () {
+      when(box.length).thenAnswer((_) => 2);
+      when(box.getAt(any)).thenAnswer((_) => data);
+      final result = repo.getArticles('');
+      expect([data, data], result);
+    });
+  });
+  test('test remove function from hive function', () {
+    ArticleBox dataToDelete = ArticleBox()
+      ..author = "author"
+      ..title = "title"
+      ..description = "description"
+      ..url = "url"
+      ..urlToImage = "urlToImage"
+      ..publishedAt = "publishedAt"
+      ..content = "content";
+    when(box.length).thenAnswer((_) => 2);
+    when(box.getAt(0)).thenAnswer((_) => data);
+    when(box.getAt(1)).thenAnswer((realInvocation) => dataToDelete);
+    repo.removeFromHive(dataToDelete);
+    verify(box.deleteAt(1));
   });
 }
